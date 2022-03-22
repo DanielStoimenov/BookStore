e5d3653f4dfa196c1b20b9f8c8bde8e23c35f27b
Daniel Stoimenov <daniel.stoimenov@itsoft.bg>
2022-03-22 10:21:19 +0200

    Implement Create/Edit action in CategoryController

---
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -0,0 +1,82 @@
+﻿using BookStoreWeb.Data;
+using BookStoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _db.Categories.ToList();
+            return View(categories);
+        }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Name cannot exactly match with the DisplayOrder");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.Find(id);
+            // var categoryFirst = _db.Categories.FirstOrDefault(x => x.Id == id);
+            // var categorySignle = _db.Categories.SingleOrDefault(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Name cannot exactly match with the DisplayOrder");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
--- BookStoreWeb/Models/Category.cs
+++ BookStoreWeb/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿namespace BookStoreWeb.Models
 {
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
 
     public class Category
@@ -10,6 +11,8 @@
         [Required]
         public string Name { get; set; }
 
+        [DisplayName("Display Order")]
+        [Range(1, 200, ErrorMessage = "Display Order must be between 1 and 200 only!")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
